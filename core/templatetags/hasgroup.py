19c7a4f0db0b5194bd4109a0b23de07c70923db0
Simon <simon@nimos.ws>
2015-02-28 22:38:52 +0100

    non-users dont have groups

--- core/templatetags/hasgroup.py
+++ core/templatetags/hasgroup.py
@@ -6,4 +6,6 @@ register = template.Library()
 @register.filter(name='has_group') 
 def has_group(user, group_name): 
 	group = Group.objects.get_or_create(name=group_name)[0] 
+	if user == None:
+		return False
 	return True if group in user.groups.all() else False
