2880a0d6ffd9a98aa24f0acf567d21293688c3d6
Simon <simon@nimos.ws>
2015-04-26 04:44:34 +0200

    Fixed a bug where users would add the same API key multiple times when submitting the registration form multiple times.

--- core/views.py
+++ core/views.py
@@ -82,7 +82,7 @@ def register(request):
 					try:
 						newUser.save()
 					except IntegrityError as e:
-						newUser = User.objects.get(username=slugify(mainChar["charName"]))
+						return redirect("core:evesso")
 
 					newProfile = UserProfile.objects.get_or_create(user=newUser)[0]
 					try:
