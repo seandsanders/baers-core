1492585499760e7d3fb4c4cf6afd3691106d6963
Simon <simon@nimos.ws>
2015-08-21 23:42:54 +0200

    Updated recruitment stuff

--- applications/__init__.py
+++ applications/__init__.py
@@ -3,16 +3,20 @@ questions = [
 	{"text": "What's your first name?", "type": "short"},
 	{"text": "What's your first language?", "type": "short"},
 	{"text": "How long have you been in BRAVE and why is BRAVE the best alliance?", "type": "long"},
+	{"text": "Why are you leaving your current corp (or alliance, if applicable)?", "type": "long"},
+	{"text": "Why do you want to join BAERS?", "type": "long"},
+	{"text": "What kind of combat experience do you have in EVE?", "type": "long"},
+	{"text": "What kind of wormhole experience do you have?", "type": "long"},
 	{"text": "What's your favourite activity in EVE?", "type": "long"},
-	{"text": "What's your favourite ship and why, also what is your most powerful ship and why?", "type": "long"},
+	{"text": "What's your favourite ship and why? What is your most powerful ship and why?", "type": "long"},
 	{"text": "Provide a good fit (<a target='_blank' href='http://wiki.eveuniversity.org/Guide_to_using_EFT'>EFT format</a>) for the ship you chose as your favorite", "type": "long"},
 	{"text": "Provide a good fit (<a target='_blank' href='http://wiki.eveuniversity.org/Guide_to_using_EFT'>EFT format</a>) for the ship you chose as your most powerful", "type": "long"},
 	{"text": "How long have you been playing EVE and what have you done so far?", "type": "long"},
 	{"text": "Provide an explain how you make isk to fund your activities.", "type": "long"},
 	{"text": "How do you go about finding content for yourself and others?", "type": "long"},	
 	{"text": "How have you demonstrated your ability to be a leader within EVE.", "type": "long"},
-	{"text": "Why do you want to join BAERS?", "type": "long"},
-	{"text": "Please write a haiku about patriots sexual prowess", "type": "long"},
+	{"text": "Are you willing to step up and lead fleets, regardless of experience, to create content for your corpmates?", "type": "short"},
+	{"text": "Please write a haiku about a DAMN PATRIOT's sexual prowess", "type": "long"},
 ]
 
 from core.tasks import tasks
