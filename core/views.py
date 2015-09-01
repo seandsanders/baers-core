013ebfb523d6f4066f6795d8fd97557b99c7aa55
Simon <simon@nimos.ws>
2015-09-02 00:38:36 +0200

    fix part 3

--- core/views.py
+++ core/views.py
@@ -642,12 +642,12 @@ def assetScan(request):
 				asset.flag = cur.fetchone();
 
 				if asset.parentID:
-					asset.parentName = parentNames.get(asset.parentID, "-")
+					asset.parentName = str(parentNames.get(asset.parentID, "-"))
 					p = CorpAsset.objects.filter(itemID=asset.parentID)
 					if p:
 						parentType = CCPinvType.objects.filter(typeID=p.first().typeID)
 						if parentType:
-							str(asset.parentName) += " ("+str(parentType.first().typeName)+")"
+							asset.parentName += " ("+str(parentType.first().typeName)+")"
 
 
 				rcAssets.append(asset)
