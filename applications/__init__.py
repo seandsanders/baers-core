fa217abd89bf575da3990aabee3b56cf9242e76c
Simon <simon@nimos.ws>
2015-09-03 03:12:59 +0200

    application questions

--- applications/__init__.py
+++ applications/__init__.py
@@ -2,7 +2,6 @@ questions = [
 	{"text": "How old are you?", "type": "short"},
 	{"text": "What's your first name?", "type": "short"},
 	{"text": "What's your first language?", "type": "short"},
-	{"text": "How long have you been in BRAVE and why is BRAVE the best alliance?", "type": "long"},
 	{"text": "Why are you leaving your current corp (or alliance, if applicable)?", "type": "long"},
 	{"text": "Why do you want to join BAERS?", "type": "long"},
 	{"text": "What kind of combat experience do you have in EVE?", "type": "long"},
@@ -12,12 +11,14 @@ questions = [
 	{"text": "Provide a good fit (<a target='_blank' href='http://wiki.eveuniversity.org/Guide_to_using_EFT'>EFT format</a>) for the ship you chose as your favorite", "type": "long"},
 	{"text": "Provide a good fit (<a target='_blank' href='http://wiki.eveuniversity.org/Guide_to_using_EFT'>EFT format</a>) for the ship you chose as your most powerful", "type": "long"},
 	{"text": "How long have you been playing EVE and what have you done so far?", "type": "long"},
-	{"text": "Provide an explain how you make isk to fund your activities.", "type": "long"},
+	{"text": "Provide an explanation of how you make isk to fund your activities.", "type": "long"},
 	{"text": "How do you go about finding content for yourself and others?", "type": "long"},	
-	{"text": "How have you demonstrated your ability to be a leader within EVE.", "type": "long"},
+	{"text": "How have you demonstrated your ability to be a leader within EVE?", "type": "long"},
 	{"text": "Are you willing to step up and lead fleets, regardless of experience, to create content for your corpmates?", "type": "short"},
 	{"text": "Please write a haiku about a DAMN PATRIOT's sexual prowess", "type": "long"},
 ]
+
+
 
 from core.tasks import tasks
 
