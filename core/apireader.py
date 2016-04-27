e4b70e4ddf69a9e285ce8b2b45b327214c4e1705
seandsanders <sean.d.sanders@gmail.com>
2016-04-27 17:23:02 +0000

    Edited apireader.py to add new full access mask.

--- core/apireader.py
+++ core/apireader.py
@@ -373,7 +373,7 @@ def refreshKeyInfo(key, full=True):
 	else:
 		key.expiration = datetime.datetime.fromtimestamp(expires)
 
-	if not key.accessMask in [268435455, 1073741823]:
+	if not key.accessMask in [268435455, 1073741823, 4294967295]:
 		if not key.valid:
 			return
 		key.valid = False
@@ -849,7 +849,7 @@ def validateKey(keyID, vCode):
 
 	if (result.key.expires != ""):
 		return None, "The Key you provided has an expiration date set."
-	if not result.key.accessMask in [268435455, 1073741823]:
+	if not result.key.accessMask in [268435455, 1073741823, 4294967295]:
 		return None, "The Key you provided is not a full access key."
 	if result.key.type != "Account":
 		return None, "The Key you provided is not a full Account key."
